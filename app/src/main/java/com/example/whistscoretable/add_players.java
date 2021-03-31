d370dad877724406dd31413b53620a71dfef9373
ClaudiuVisan <visanclaudiu2000@tutanota.com>
2021-03-31 14:29:41 +0300

    Primele 2  activitati, de facut remove la schimbarea numarului de jucatori

--- app/src/main/java/com/example/whistscoretable/add_players.java
+++ app/src/main/java/com/example/whistscoretable/add_players.java
@@ -4,18 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 public class add_players extends AppCompatActivity {
+    private int noPlayers;
     LinearLayout myLayout=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
         Intent intent = getIntent();
-        int noPlayers=intent.getIntExtra("noPlayers",3);
+        Button placeHolder=(Button) findViewById(R.id.placeHolder);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        placeHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String noPlayersstr=String.valueOf(spinner.getSelectedItem()) ;
+                noPlayers=Integer.parseInt(noPlayersstr);
+                addPlayers();
+            }
+        });
+    }
+
+    public void addPlayers(){
         int i;
         for(i=1;i<=noPlayers;i++)
         {
@@ -25,9 +40,10 @@ public class add_players extends AppCompatActivity {
                     LinearLayout.LayoutParams.WRAP_CONTENT
             );
             EditText caseta = new EditText(add_players.this);
-            caseta.setText("Jucatorul "+i);
+            caseta.setText("Player "+i);
             caseta.setId(i);
             myLayout.addView(caseta, myParams);
         }
+
     }
 }
