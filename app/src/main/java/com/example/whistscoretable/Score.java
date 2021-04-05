dee8a4b44784d6c4ccf850b894288217cc1c4df0
HermanBob <raduwagner0@gmail.com>
2021-04-05 17:05:36 +0300

    Bag pula

--- app/src/main/java/com/example/whistscoretable/Score.java
+++ app/src/main/java/com/example/whistscoretable/Score.java
@@ -2,6 +2,9 @@ package com.example.whistscoretable;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,5 +17,27 @@ public class Score extends AppCompatActivity {
         setContentView(R.layout.activity_score);
         ArrayList<Player> playerList = (ArrayList<Player>) getIntent().getSerializableExtra("playerList");
         int noPlayers=(getIntent().getIntExtra("noPlayers",2));
+        TableLayout tabel = (TableLayout) findViewById(R.id.tabel);
+        tabel.setColumnStretchable(1, true);
+        tabel.setColumnStretchable(2, true);
+        tabel.setStretchAllColumns(true);
+
+
+        for(int i=1;i<=noPlayers;i++)
+        {
+            TableRow rand = new TableRow(this);
+            TableRow.LayoutParams myParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            rand.setLayoutParams(myParams);
+            TextView casetNume = new TextView(this);
+            casetNume.setText(playerList.get(i-1).getName());
+
+            rand.addView(casetNume);
+            TextView casetScor = new TextView(this);
+            casetScor.setText("69");
+
+            rand.addView(casetScor);
+            tabel.addView(rand);
+
+        }
     }
 }
