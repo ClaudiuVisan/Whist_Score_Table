deeac07f3c4a87b22ba1b97193791292e7cdbc6b
ClaudiuVisan <visanclaudiu2000@tutanota.com>
2021-05-04 11:43:22 +0300

    Adaugat save icon

---
+++ app/src/main/java/com/example/whistscoretable/LoadGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.whistscoretable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_game);
+    }
+}
