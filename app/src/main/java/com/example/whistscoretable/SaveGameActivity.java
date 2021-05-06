aea3f1b55e10b504d52904e48ef9a3511ecae875
HermanBob <raduwagner0@gmail.com>
2021-05-06 16:52:08 +0300

    recycler view cu obiecte

---
+++ app/src/main/java/com/example/whistscoretable/SaveGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.whistscoretable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_game);
+    }
+}
